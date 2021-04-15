95c946b9928e6411be612cae7ce90a17401428c7
LJH's MacBookPro16 <60730765+anshqhsh@users.noreply.github.com>
2021-04-15 13:07:30 +0900

    ErrorState
    try-catch(error) 에서 error는 any타입으로 에러에 대한 어떤 처리를 해주기 위해서는 ErrorState라는 type을 생성해서 써주면 된다.

--- 8-exception/8-3-state.ts
+++ 8-exception/8-3-state.ts
@@ -5,10 +5,19 @@ class TimeoutError extends Error{
     
 }
 
+type NetworkErrorState = {
+    result : 'fail';
+    reason : 'offline' | 'down' | 'timeout';
+}
+
+type SuccessState = {
+    result : 'success';
+}
+
+type ResultState = SuccessState | NetworkErrorState
+
 class NetworkClient { 
-    tryConnect(): void{
-        throw new OfflineError('no network');
-    }
+    tryConnect(): ResultState{}
 }
 
 class UserService {
